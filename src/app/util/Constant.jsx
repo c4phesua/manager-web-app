2ce53e5d58d0b3e2a62f779a03a386c32f67f1ce
Nguyen Tri Nhan <nguyentrinhan.dev@gmail.com>
2022-02-11 15:00:15 +0700

    change error title

--- src/app/util/Constant.jsx
+++ src/app/util/Constant.jsx
@@ -13,7 +13,7 @@ export const STATUS = {
 }
 
 export const MESSAGE = {
-  NOT_FOUND: '404 not found',
+  NOT_FOUND: 'Không tìm thấy trang',
   PERMISSION_DENIED: 'Bạn không có quyền truy cập vào trang này.',
   USER_DENIED: 'Chỉ có Admin và người quản lý showroom mới có thể đăng nhập vào trang web này.',
   UNCONFIRMED_DENIED: (user) => `Tài khoản chưa được kích hoạt, vui lòng đến địa chỉ email: ${user.email} để kích hoạt tài khoản.`,
