aab39766eec9f7005f1dd05b861513e34483330c
Nguyen Tri Nhan <nguyentrinhan.dev@gmail.com>
2022-02-16 19:37:41 +0700

    export reactstrap

--- src/app/component/dialog/CreateManagerDialog.jsx
+++ src/app/component/dialog/CreateManagerDialog.jsx
@@ -1,4 +1,5 @@
 import { Button, DialogActions, DialogContent, TextField, Typography } from '@material-ui/core';
+import { Label, Form } from 'reactstrap';
 import React from 'react';
 import { useState } from 'react';
 import CloseableDialogComponent from './CloseableDialogComponent';
@@ -20,16 +21,16 @@ const CreateManagerDialog = ({ open, handleClose, ...props }) => {
 
   return (
     <CloseableDialogComponent title='Thêm quản lý' maxWidth='lg' isOpen={open}  {...props}>
-      <form onSubmit={onDialogSubmit}>
+      <Form onSubmit={onDialogSubmit}>
         <DialogContent>
           <Typography variant='h6'>Họ</Typography>
-          <TextField variant='outlined' size='small'/>
+          <TextField name='' variant='outlined' size='small'/>
         </DialogContent>
         <DialogActions>
           <Button type='submit' color='inherit' variant='contained' onClick={handleClose}>Huỷ</Button>
           <Button type='submit' color='primary' variant='contained'>Tạo</Button>
         </DialogActions>
-      </form>
+      </Form>
     </CloseableDialogComponent>
 
   );
