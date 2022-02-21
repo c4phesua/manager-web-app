81c7952d8ad8b0787a09d01a907a389d29a323f3
Nguyen Tri Nhan <nguyentrinhan.dev@gmail.com>
2022-02-21 23:53:43 +0700

    add api

--- src/app/util/Apis.jsx
+++ src/app/util/Apis.jsx
@@ -2,6 +2,7 @@
 const Apis = {
   userMe: '/user/me',
   login: '/auth/login',
+  manager: '/manager',
 }
 
 export default Apis;
--- src/app/util/Services.jsx
+++ src/app/util/Services.jsx
@@ -1,6 +1,5 @@
 import http from './http';
 import Apis from './Apis';
-import { Search } from '@material-ui/icons';
 const URL_PERFIX = process.env.REACT_APP_API;
 const Services = {
   getMe() {
@@ -13,6 +12,10 @@ const Services = {
 
   search(entity, params) {
     return http.get(`${URL_PERFIX}/${entity}`, null, params);
+  },
+
+  createManager(manager) {
+    return http.post(URL_PERFIX + Apis.manager, manager);
   }
 };
 
