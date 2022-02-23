1e210bdf256e56018cfac7267d36807ce2b6645d
Nguyen Tri Nhan <nguyentrinhan.dev@gmail.com>
2022-02-24 05:48:17 +0700

    profile dropdown

--- src/app/component/TopNavBar.jsx
+++ src/app/component/TopNavBar.jsx
@@ -1,11 +1,51 @@
-import { Toolbar, Typography } from '@material-ui/core';
+import { IconButton, List, ListItem, Toolbar, Typography } from '@material-ui/core';
 import React from 'react';
+import DropDown from './DropDown';
+import ProfileImage from './ProfileImage';
 
 const TopNavBar = ({user}) => {
+  const avatar = user?.avatar || './static/images/defaultAvatar.png';
+
+  const avatarButton = (props) => (
+    <IconButton
+      key="profile"
+      title={user.email}
+      {...props}
+    >
+      <ProfileImage
+        diameter={36}
+        name={user.email}
+        url={avatar}
+      />
+    </IconButton>
+  );
+
+  const renderDropDownAvatar = () => {
+    return (
+      <DropDown renderAnchorElement={avatarButton} >
+        <div className="top-right-dropdown">
+          <List>
+            <ListItem key="user" id="user">
+              <div className="user-profile">
+                <ProfileImage
+                  diameter={36}
+                  name={user.email}
+                  url={avatar}
+                />
+                <b>{user.firstname} {user.lastname}</b>
+                <div className="text-muted">{user.email}</div>
+              </div>
+            </ListItem>
+          </List>
+        </div>
+
+      </DropDown>
+    )
+  }
   return (
     <Toolbar>
       <div className='flex-grow-1'></div>
-      {user && <Typography variant='h5'>Xin chào, {user.firstname} {user.lastname}</Typography>}
+      {renderDropDownAvatar()}
     </Toolbar>
   );
 }
