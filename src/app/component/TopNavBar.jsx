176cf36c945490339d8028ca8100ec29e915537b
Nguyen Tri Nhan <nguyentrinhan.dev@gmail.com>
2022-02-24 08:29:43 +0700

    add logout to popover

--- src/app/component/TopNavBar.jsx
+++ src/app/component/TopNavBar.jsx
@@ -1,9 +1,12 @@
-import { IconButton, List, ListItem, Toolbar, Typography } from '@material-ui/core';
+import { Button, IconButton, List, ListItem, Toolbar } from '@material-ui/core';
+import { Backspace } from '@material-ui/icons';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import RouteConstants from '../routes/RouteConstants';
 import DropDown from './DropDown';
 import ProfileImage from './ProfileImage';
 
-const TopNavBar = ({user}) => {
+const TopNavBar = ({ user }) => {
   const avatar = user?.avatar || './static/images/defaultAvatar.png';
 
   const avatarButton = (props) => (
@@ -20,25 +23,35 @@ const TopNavBar = ({user}) => {
     </IconButton>
   );
 
+  const logoutButton = () => {
+    return (
+      <>
+        <Link to={RouteConstants.logout} >Đăng Xuất </Link>
+      </>
+    )
+  }
+
   const renderDropDownAvatar = () => {
     return (
       <DropDown renderAnchorElement={avatarButton} >
         <div className="top-right-dropdown">
           <List>
             <ListItem key="user" id="user">
-              <div className="user-profile">
-                <ProfileImage
-                  diameter={36}
-                  name={user.email}
-                  url={avatar}
-                />
+              <div className="user-profile d-flex flex-column">
+                <div className="align-self-center">
+                  <ProfileImage
+                    diameter={36}
+                    name={user.email}
+                    url={avatar}
+                  />
+                </div>
                 <b>{user.firstname} {user.lastname}</b>
                 <div className="text-muted">{user.email}</div>
+                {logoutButton()}
               </div>
             </ListItem>
           </List>
         </div>
-
       </DropDown>
     )
   }
