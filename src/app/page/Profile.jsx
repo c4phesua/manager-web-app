920297e20865369107e1736ea5a73bc6f4248f00
Nguyen Tri Nhan <nguyentrinhan.dev@gmail.com>
2022-02-25 17:41:29 +0700

    update profile

--- src/app/page/Profile.jsx
+++ src/app/page/Profile.jsx
@@ -1,3 +1,4 @@
+import { Typography } from '@material-ui/core';
 import React from 'react';
 import { PAGE_NAME } from '../util/Constant';
 
@@ -5,9 +6,10 @@ const Profile = (props) => {
 
   document.title = PAGE_NAME.PROFILE;
 
+  const { user } = props;
   return (
     <div>
-      
+      <Typography variant='h3' align='left'>{PAGE_NAME.PROFILE}</Typography>
     </div>
   );
 }
