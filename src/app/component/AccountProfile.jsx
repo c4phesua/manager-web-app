7238c51fe876b2e8f639a7678c4ce730e2b2dae3
Nguyen Tri Nhan <nguyentrinhan.dev@gmail.com>
2022-02-27 20:54:53 +0700

    add profile

---
+++ src/app/component/AccountProfile.jsx
@@ -0,0 +1,61 @@
+import { Avatar, Box, Button, Card, CardActions, CardContent, Divider, Typography } from "@material-ui/core";
+
+const AccountProfile = (props) => {
+
+  const { user } = props;
+  const avatar = user?.avatar ? user.avatar : './static/images/defaultAvatar.png';
+
+  return (
+    <Card {...props}>
+      <CardContent>
+        <Box
+          sx={{
+            alignItems: 'center',
+            display: 'flex',
+            flexDirection: 'column'
+          }}
+        >
+          <Avatar
+            src={avatar}
+            sx={{
+              height: 64,
+              mb: 2,
+              width: 64
+            }}
+          />
+          <Typography
+            color="textPrimary"
+            gutterBottom
+            variant="h5"
+          >
+            {user.firstname} {user.lastname}
+          </Typography>
+          <Typography
+            color="textSecondary"
+            variant="body2"
+          >
+            {`${user.address}`}
+          </Typography>
+          {/* <Typography
+            color="textSecondary"
+            variant="body2"
+          >
+            {user.timezone}
+          </Typography> */}
+        </Box>
+      </CardContent>
+      <Divider />
+      <CardActions>
+        <Button
+          color="primary"
+          fullWidth
+          variant="text"
+        >
+          Đổi ảnh đại diện
+        </Button>
+      </CardActions>
+    </Card>
+  )
+};
+
+export default AccountProfile;
--- src/app/page/Profile.jsx
+++ src/app/page/Profile.jsx
@@ -1,15 +1,47 @@
-import { Typography } from '@material-ui/core';
+import { Box, Container, Grid, Typography } from '@material-ui/core';
 import React from 'react';
+import AccountProfile from '../component/AccountProfile';
+import { AccountProfileDetails } from '../component/AccountProfileDetails';
 import { PAGE_NAME } from '../util/Constant';
 
 const Profile = (props) => {
 
   document.title = PAGE_NAME.PROFILE;
-
   const { user } = props;
   return (
     <div>
       <Typography variant='h3' align='left'>{PAGE_NAME.PROFILE}</Typography>
+      <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+        py: 8
+      }}
+    >
+      <Container maxWidth="lg">
+        <Grid
+          container
+          spacing={3}
+        >
+          <Grid
+            item
+            lg={4}
+            md={6}
+            xs={12}
+          >
+            <AccountProfile user={user} />
+          </Grid>
+          <Grid
+            item
+            lg={8}
+            md={6}
+            xs={12}
+          >
+            <AccountProfileDetails user={user} />
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
     </div>
   );
 }
